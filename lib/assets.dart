a6b7f2e29e047ece0a1c3b476a344c8cb1635385
Hany Medhat <hany.medhat24@gmail.com>
2023-08-18 22:33:01 +0300

    create class assets

---
+++ lib/assets.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for Note1
+  /// assets/note1.wav
+  static const String Note1 = "assets/note1.wav";
+
+  /// Assets for Note2
+  /// assets/note2.wav
+  static const String Note2 = "assets/note2.wav";
+
+  /// Assets for Note3
+  /// assets/note3.wav
+  static const String Note3 = "assets/note3.wav";
+
+  /// Assets for Note4
+  /// assets/note4.wav
+  static const String Note4 = "assets/note4.wav";
+
+  /// Assets for Note5
+  /// assets/note5.wav
+  static const String Note5 = "assets/note5.wav";
+
+  /// Assets for Note6
+  /// assets/note6.wav
+  static const String Note6 = "assets/note6.wav";
+
+  /// Assets for Note7
+  /// assets/note7.wav
+  static const String Note7 = "assets/note7.wav";
+}
